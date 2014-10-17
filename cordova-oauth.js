e718d0e9f53e7f0a4e9e99c1d1c81b3e6c236a2e
Nic Raboy <nraboy@gmail.com>
2014-10-16 19:49:17 -0700

    initial commit.  digital ocean and dropbox support.

---
+++ cordova-oauth.js
@@ -0,0 +1,104 @@
+/*
+ * Cordova Oauth
+ *
+ * Created by Nic Raboy
+ * http://www.nraboy.com
+ *
+ *
+ *
+ * DESCRIPTION:
+ *
+ * Use Oauth sign in for various web services.
+ *
+ *
+ * REQUIRES:
+ *
+ *    Apache Cordova 3.5+
+ *    Apache InAppBrowser Plugin
+ *
+ *
+ * SUPPORTS:
+ *
+ *    Dropbox
+ *    Digital Ocean
+ */
+
+(function(){
+
+    angular.module("cordovaOauth", []).factory('$cordovaOauth', ['$q', function ($q, $http) {
+
+        return {
+
+            /*
+             * Sign into the Dropbox service
+             *
+             * @param    string appKey
+             * @return   promise
+             */
+            dropbox: function(appKey) {
+                var deferred = $q.defer();
+                if(window.cordova) {
+                    var browserRef = window.open("https://www.dropbox.com/1/oauth2/authorize?client_id=" + appKey + "&redirect_uri=http://localhost/callback" + "&response_type=token", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+                    browserRef.addEventListener("loadstart", function(event) {
+                        if((event.url).startsWith("http://localhost/callback")) {
+                            var callbackResponse = (event.url).split("#")[1];
+                            var responseParameters = (callbackResponse).split("&");
+                            var parameterMap = [];
+                            for(var i = 0; i < responseParameters.length; i++) {
+                                parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+                            }
+                            if(parameterMap["access_token"] !== undefined && parameterMap["access_token"] !== null) {
+                                var promiseResponse = {
+                                    access_token: parameterMap["access_token"],
+                                    token_type: parameterMap["token_type"],
+                                    uid: parameterMap["uid"]
+                                }
+                                deferred.resolve(promiseResponse);
+                            } else {
+                                deferred.reject("Problem authenticating");
+                            }
+                            browserRef.close();
+                        }
+                    });
+                } else {
+                    deferred.reject("Cannot authenticate via a web browser");
+                }
+                return deferred.promise;
+            },
+
+            /*
+             * Sign into the Digital Ocean service
+             *
+             * @param    string clientId
+             * @param    string clientSecret
+             * @return   promise
+             */
+            digitalOcean: function(clientId, clientSecret) {
+                var deferred = $q.defer();
+                if(window.cordova) {
+                    var browserRef = window.open("https://cloud.digitalocean.com/v1/oauth/authorize?client_id=" + clientId + "&redirect_uri=http://localhost/callback&response_type=code&scope=read%20write", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+                    browserRef.addEventListener("loadstart", function(event) {
+                        if((event.url).startsWith("http://localhost/callback")) {
+                            var requestToken = (event.url).split("code=")[1];
+                            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+                            $http({method: "post", url: "https://cloud.digitalocean.com/v1/oauth/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
+                                .success(function(data) {
+                                    deferred.resolve(data);
+                                })
+                                .error(function(data, status) {
+                                    deferred.reject("Problem authenticating");
+                                });
+                            browserRef.close();
+                        }
+                    });
+                } else {
+                    deferred.reject("Cannot authenticate via a web browser");
+                }
+                return deferred.promise;
+            }
+
+        }
+
+    }]);
+
+})();
