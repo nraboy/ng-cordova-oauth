983c5c96ba9445f61542e159fa43a6a1f1902729
Nic Raboy <nraboy@gmail.com>
2014-10-19 10:42:51 -0700

    add Google service to the list of Oauth providers

--- README.md
+++ README.md
@@ -40,13 +40,14 @@ Each web service API acts independently in this library.  However, when configur
 consistent.  You must use **http://localhost/callback** as your callback / redirect URI.  This is because this library will 
 perform tasks when this URL is found.
 
-    $cordovaOauth.dropbox(appKey);
-    $cordovaOauth.digitalOcean(clientId, clientSecret);
+    $cordovaOauth.dropbox(string appKey);
+    $cordovaOauth.digitalOcean(string clientId, string clientSecret);
+    $cordovaOauth.google(string clientId, string clientSecret, array appScope);
 
 Each API call returns a promise.  The success callback will provide a response object and the error 
 callback will return a string.
 
-    $cordovaOauth.dropbox("[APP_KEY_HERE]").then(function(result) {
+    $cordovaOauth.google("CLIENT_ID_HERE", "CLIENT_SECRET_HERE", ["https://www.googleapis.com/auth/userinfo.email"]).then(function(result) {
         console.log("Response Object -> " + JSON.stringify(result));
     }, function(error) {
         console.log("Error -> " + error);
@@ -60,6 +61,7 @@ Version History
 
 * Add Digital Ocean support
 * Add Dropbox support
+* Add Google support
 
 
 Have a question or found a bug (compliments work too)?
--- ng-cordova-oauth.js
+++ ng-cordova-oauth.js
@@ -21,6 +21,7 @@
  *
  *    Dropbox
  *    Digital Ocean
+ *    Google
  */
 
 (function(){
@@ -82,6 +83,38 @@
                             var requestToken = (event.url).split("code=")[1];
                             $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
                             $http({method: "post", url: "https://cloud.digitalocean.com/v1/oauth/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
+                                .success(function(data) {
+                                    deferred.resolve(data);
+                                })
+                                .error(function(data, status) {
+                                    deferred.reject("Problem authenticating");
+                                });
+                            browserRef.close();
+                        }
+                    });
+                } else {
+                    deferred.reject("Cannot authenticate via a web browser");
+                }
+                return deferred.promise;
+            },
+
+            /*
+             * Sign into the Google service
+             *
+             * @param    string clientId
+             * @param    string clientSecret
+             * @param    array appScope
+             * @return   promise
+             */
+            google: function(clientId, clientSecret, appScope) {
+                var deferred = $q.defer();
+                if(window.cordova) {
+                    var browserRef = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+                    browserRef.addEventListener('loadstart', function(event) { 
+                        if((event.url).indexOf("http://localhost/callback") == 0) {
+                            requestToken = (event.url).split("code=")[1];
+                            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+                            $http({method: "post", url: "https://accounts.google.com/o/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=http://localhost/callback" + "&grant_type=authorization_code" + "&code=" + requestToken })
                                 .success(function(data) {
                                     deferred.resolve(data);
                                 })
