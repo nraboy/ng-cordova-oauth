2db9d2b9272bd6cc5b0796b4e9ef3f252f97d81c
Christopher Fitzner <chrisf@extrahop.com>
2014-12-09 19:29:20 -0800

    Add Withings support

--- ng-cordova-oauth.js
+++ ng-cordova-oauth.js
@@ -32,6 +32,7 @@
  *    Meetup
  *    Salesforce
  *    Strava
+ *    Withings
  */
 
 (function(){
@@ -592,8 +593,101 @@
                     deferred.reject("Cannot authenticate via a web browser");
                 }
                 return deferred.promise;
-            }
+            },
 
+            /*
+             * Sign into the Withings service
+             * Note that this service requires jsSHA for generating HMAC-SHA1 Oauth 1.0 signatures
+             *
+             * @param    string clientId
+             * @param    string clientSecret
+             * @return   promise
+             */
+            withings: function(clientId, clientSecret) {
+                var deferred = $q.defer();
+                if(window.cordova) {
+                    var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
+                    if(cordovaMetadata.hasOwnProperty("org.apache.cordova.inappbrowser") === true) {
+                        if(typeof jsSHA !== "undefined") {
+
+                            // Step 1 : get a oAuth "request token"
+                            var oauthObject = $cordovaOauthUtility.generateOauthParametersInstance(clientId);
+                            oauthObject.oauth_callback = 'http://localhost/callback';
+
+                            var requestTokenUrlBase = "https://oauth.withings.com/account/request_token";
+                            var signatureObj = $cordovaOauthUtility.createSignature("GET", requestTokenUrlBase, {}, oauthObject, clientSecret);
+                            oauthObject.oauth_signature = signatureObj.signature;
+
+                            var requestTokenParameters = $cordovaOauthUtility.generateUrlParameters(oauthObject);
+
+                            $http({method: "get", url: requestTokenUrlBase + "?" + requestTokenParameters })
+                                .success(function(requestTokenResult) {
+
+                                    // Step 2 : End-user authorization
+                                    var parameterMap = $cordovaOauthUtility.parseResponseParameters(requestTokenResult);
+                                    if(parameterMap.hasOwnProperty("oauth_token") === false) {
+                                        deferred.reject("Oauth request token was not received");
+                                    }
+                                    var oauthObject = $cordovaOauthUtility.generateOauthParametersInstance(clientId);
+                                    oauthObject.oauth_token = parameterMap.oauth_token;
+
+                                    // used in step 3
+                                    var oauthTokenSecret = parameterMap.oauth_token_secret;
+
+                                    var authorizeUrlBase = "https://oauth.withings.com/account/authorize";
+                                    var signatureObj = $cordovaOauthUtility.createSignature("GET", authorizeUrlBase, {}, oauthObject, clientSecret);
+                                    oauthObject.oauth_signature = signatureObj.signature;
+
+                                    var authorizeParameters = $cordovaOauthUtility.generateUrlParameters(oauthObject);
+                                    var browserRef = window.open(authorizeUrlBase + '?' + authorizeParameters, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+
+                                    // STEP 3: User Data Access token
+                                    browserRef.addEventListener('loadstart', function(event) {
+                                        if((event.url).indexOf("http://localhost/callback") === 0) {
+                                            var callbackResponse = (event.url).split("?")[1];
+                                            parameterMap = $cordovaOauthUtility.parseResponseParameters(callbackResponse);
+                                            if(parameterMap.hasOwnProperty("oauth_verifier") === false) {
+                                                deferred.reject("Browser authentication failed to complete.  No oauth_verifier was returned");
+                                            }
+
+                                            var oauthObject = $cordovaOauthUtility.generateOauthParametersInstance(clientId);
+                                            oauthObject.oauth_token = parameterMap.oauth_token;
+
+                                            var accessTokenUrlBase = "https://oauth.withings.com/account/access_token";
+                                            var signatureObj = $cordovaOauthUtility.createSignature("GET", accessTokenUrlBase, {}, oauthObject, clientSecret, oauthTokenSecret);
+                                            oauthObject.oauth_signature = signatureObj.signature;
+
+                                            var accessTokenParameters = $cordovaOauthUtility.generateUrlParameters(oauthObject);
+
+                                            $http({method: "get", url: accessTokenUrlBase + '?' + accessTokenParameters})
+                                                .success(function(result) {
+                                                    var parameterMap = $cordovaOauthUtility.parseResponseParameters(result);
+                                                    if(parameterMap.hasOwnProperty("oauth_token_secret") === false) {
+                                                        deferred.reject("Oauth access token was not received");
+                                                    }
+                                                    deferred.resolve(parameterMap);
+                                                })
+                                                .error(function(error) {
+                                                    deferred.reject(error);
+                                                });
+                                            browserRef.close();
+                                        }
+                                    });
+                                })
+                                .error(function(error) {
+                                    deferred.reject(error);
+                                });
+                        } else {
+                            deferred.reject("Missing jsSHA JavaScript library");
+                        }
+                    } else {
+                        deferred.reject("Could not find InAppBrowser plugin");
+                    }
+                } else {
+                    deferred.reject("Cannot authenticate via a web browser");
+                }
+                return deferred.promise;
+            }
         };
 
     }]);
@@ -671,8 +765,48 @@
                     text += possible.charAt(Math.floor(Math.random() * possible.length));
                 }
                 return text;
-            }
+            },
 
+            generateUrlParameters: function (parameters) {
+                var sortedKeys = Object.keys(parameters);
+                sortedKeys.sort();
+
+                var params = "";
+                var amp = "";
+
+                for (var i = 0 ; i < sortedKeys.length; i++) {
+                    params += amp + sortedKeys[i] + "=" + parameters[sortedKeys[i]];
+                    amp = "&";
+                }
+
+                return params;
+            },
+
+            parseResponseParameters: function (response) {
+                if (response.split) {
+                    var parameters = response.split("&");
+                    var parameterMap = {};
+                    for(var i = 0; i < parameters.length; i++) {
+                        parameterMap[parameters[i].split("=")[0]] = parameters[i].split("=")[1];
+                    }
+                    return parameterMap;
+                }
+                else {
+                    return {};
+                }
+            },
+
+            generateOauthParametersInstance: function(consumerKey) {
+                var nonceObj = new jsSHA(Math.round((new Date()).getTime() / 1000.0), "TEXT");
+                var oauthObject = {
+                    oauth_consumer_key: consumerKey,
+                    oauth_nonce: nonceObj.getHash("SHA-1", "HEX"),
+                    oauth_signature_method: "HMAC-SHA1",
+                    oauth_timestamp: Math.round((new Date()).getTime() / 1000.0),
+                    oauth_version: "1.0"
+                };
+                return oauthObject;
+            }
         };
 
     }]);
