7b908502902d2a0909067b00bd6622bb4302bf30
Nic Raboy <nraboy@gmail.com>
2015-01-12 19:52:20 -0800

    see if i can style the gh-pages

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var oauthApp = angular.module("oauthapp", ["ui.router"]);
+
+oauthApp.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('docs', {
+            url: '/docs',
+            templateUrl: 'templates/docs.html'
+        })
+        .state('docs.providers', {
+            url: '/providers',
+            templateUrl: 'templates/providers.html',
+            controller: 'ProvidersController'
+        })
+        .state('docs.provider', {
+            url: '/provider/:providerName',
+            templateUrl: 'templates/provider.html',
+            controller: 'ProviderController'
+        });
+    $urlRouterProvider.otherwise('/docs/providers');
+});
+
+
+oauthApp.controller("ProvidersController", function($scope) {
+
+});
+
+oauthApp.controller("ProviderController", function($scope) {
+
+});
