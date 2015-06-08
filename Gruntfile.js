8e799e5e8a806038840d5f25965ad498c1a092f9
akshayrangnekar <akshay@annkh.com>
2015-06-08 16:07:40 +0100

    Added non-minified production file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,15 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+            },
+            dist: {
+                src: ['src/*.js'],
+                dest: '<%= pkg.name %>.js',
+            },
+        },
+
         uglify: {
             options: {
                 banner: '// <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n' + '// http://www.nraboy.com\n'
@@ -22,8 +31,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     grunt.registerTask('test', ['clean', 'jshint']);
-    grunt.registerTask('default', ['clean', 'jshint', 'uglify']);
+    grunt.registerTask('default', ['clean', 'jshint', 'concat', 'uglify']);
 
 };
