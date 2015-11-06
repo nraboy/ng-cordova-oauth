572b035a5a579c56ae364311c56a02188a2efd1b
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-06 09:47:11 -0300

    Adding karma to test task on grunt file
    Also format grunt file to two spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,45 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-            },
-            dist: {
-                src: ['src/*.js'],
-                dest: 'dist/<%= pkg.name %>.js',
-            },
-        },
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+      },
+      dist: {
+        src: ['src/*.js'],
+        dest: 'dist/<%= pkg.name %>.js',
+      },
+    },
 
-        uglify: {
-            options: {
-                banner: '// <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n' + '// http://www.nraboy.com\n'
-            },
-            build: {
-                src: ['src/*.js'],
-                dest: 'dist/<%= pkg.name %>.min.js'
-            }
-        },
-        jshint: {
-            all: ['src/*.js']
-        },
-        clean: {
-            js: ['dist/*.min.js']
-        }
-    });
+    uglify: {
+      options: {
+        banner: '// <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n' + '// http://www.nraboy.com\n'
+      },
+      build: {
+        src: ['src/*.js'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      all: ['src/*.js']
+    },
+    clean: {
+      js: ['dist/*.min.js']
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('test', ['clean', 'jshint']);
-    grunt.registerTask('default', ['clean', 'jshint', 'concat', 'uglify']);
+  grunt.registerTask('test', ['clean', 'jshint', 'karma']);
+  grunt.registerTask('default', ['clean', 'jshint', 'karma', 'concat', 'uglify']);
 
 };
