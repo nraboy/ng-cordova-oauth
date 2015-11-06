38f6ee76cc44b1d7596a918f89dee0767bf0eeb6
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2015-11-05 23:00:38 -0300

    Creating first tests
    Testing function isInAppBrowserInstalled

---
+++ tests/utility.test.js
@@ -0,0 +1,36 @@
+describe('oauth.utils tests', function() {
+  beforeEach(module('ngCordovaOauth'));
+
+  var oauthUtils;
+
+  beforeEach(inject(function($cordovaOauthUtility) {
+    oauthUtils = $cordovaOauthUtility;
+  }));
+
+  describe('isInAppBrowserInstalled', function() {
+    // Mocks
+    var oldPluginMetadata, newPluginMetadata, nonePluginMetadata;
+    beforeEach(function() {
+      oldPluginMetadata = {
+        "org.apache.cordova.inappbrowser": "1.0.0"
+      }
+      newPluginMetadata = {
+        "cordova-plugin-inappbrowser": "2.0.0"
+      }
+      nonePluginMetadata = {}
+    });
+
+
+    it('Should return false if plugin not installed', function() {
+      expect(oauthUtils.isInAppBrowserInstalled(nonePluginMetadata)).toBe(false);
+    });
+
+    it('Should return true when the old version of plugin is installed', function() {
+      expect(oauthUtils.isInAppBrowserInstalled(oldPluginMetadata)).toBe(true);
+    });
+
+    it('Should return true when the new version of plugin is installed', function() {
+      expect(oauthUtils.isInAppBrowserInstalled(newPluginMetadata)).toBe(true);
+    });
+  });
+});
