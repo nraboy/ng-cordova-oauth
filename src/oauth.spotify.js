7f37c348ede6966db15db96ac08ed1d0157a7bed
PythagoRascal <pythagorascal+github@gmail.com>
2015-12-10 10:10:21 +0100

    Add support for Authorization Code Flow for Spotify API

--- src/oauth.spotify.js
+++ src/oauth.spotify.js
@@ -16,12 +16,24 @@ function spotify($q, $http, $cordovaOauthUtility) {
     if(window.cordova) {
       if($cordovaOauthUtility.isInAppBrowserInstalled()) {
         var redirect_uri = "http://localhost/callback";
+        var response_type = "token";
+        var state = "";
+        var show_dialog = false;
         if(options !== undefined) {
           if(options.hasOwnProperty("redirect_uri")) {
             redirect_uri = options.redirect_uri;
           }
+          if(options.hasOwnProperty("response_type")) {
+            response_type = options.response_type;
+          }
+          if(options.hasOwnProperty("state")) {
+            state = "&state=" + options.state;
+          }
+          if(options.hasOwnProperty("show_dialog")) {
+            show_dialog = "&show_dialog=" + options.show_dialog;
+          }
         }
-        var browserRef = window.cordova.InAppBrowser.open('https://accounts.spotify.com/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&response_type=token&scope=' + appScope.join(" "), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+        var browserRef = window.cordova.InAppBrowser.open('https://accounts.spotify.com/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&response_type=' + response_type + state + '&scope=' + appScope.join(" ") + show_dialog, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
         browserRef.addEventListener('loadstart', function(event) {
           if((event.url).indexOf(redirect_uri) === 0) {
             browserRef.removeEventListener("exit",function(event){});
@@ -32,8 +44,10 @@ function spotify($q, $http, $cordovaOauthUtility) {
             for(var i = 0; i < responseParameters.length; i++) {
               parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
             }
-            if(parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
+            if(response_type === "token" && parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
               deferred.resolve({ access_token: parameterMap.access_token, expires_in: parameterMap.expires_in, account_username: parameterMap.account_username });
+            } else if(response_type === "code" && parameterMap.code !== undefined && parameterMap.code !== null) {
+              deferred.resolve({ code: parameterMap.code, state: parameterMap.state });
             } else {
               deferred.reject("Problem authenticating");
             }
