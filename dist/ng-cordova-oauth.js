f3f8d285f1f6051ac68a22ad4c0fc47558770234
PythagoRascal <pythagorascal+github@gmail.com>
2015-12-10 11:47:19 +0100

    Fixed response parameter parsing

--- dist/ng-cordova-oauth.js
+++ dist/ng-cordova-oauth.js
@@ -1700,7 +1700,8 @@ function spotify($q, $http, $cordovaOauthUtility) {
           if((event.url).indexOf(redirect_uri) === 0) {
             browserRef.removeEventListener("exit",function(event){});
             browserRef.close();
-            var callbackResponse = (event.url).split("#")[1];
+            var splitChar = (response_type === "code") ? "#" : "?";
+            var callbackResponse = (event.url).split(splitChar)[1];
             var responseParameters = (callbackResponse).split("&");
             var parameterMap = [];
             for(var i = 0; i < responseParameters.length; i++) {
