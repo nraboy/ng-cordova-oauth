de0736d6580ad56a0e5abf0b0ed1c2945e4a0a6e
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2016-03-16 15:16:45 -0300

    IIFE rally

--- src/oauth.rally.js
+++ src/oauth.rally.js
@@ -1,59 +1,63 @@
-angular.module('oauth.rally', ['oauth.utils'])
-  .factory('$ngCordovaRally', rally);
+(function() {
+  'use strict';
 
-function rally($q, $http, $cordovaOauthUtility) {
-  return { signin: oauthRally };
+  angular.module('oauth.rally', ['oauth.utils'])
+    .factory('$ngCordovaRally', rally);
 
-  /*
-   * Sign into the Rally service
-   *
-   * @param    string clientId
-   * @param    string clientSecret
-   * @param    string appScope
-   * @param    object options
-   * @return   promise
-   */
-  function oauthRally(clientId, clientSecret, appScope, options) {
-    var deferred = $q.defer();
-    if(window.cordova) {
-        if($cordovaOauthUtility.isInAppBrowserInstalled()) {
-          var redirect_uri = "http://localhost/callback";
-          if(options !== undefined) {
-            if(options.hasOwnProperty("redirect_uri")) {
-              redirect_uri = options.redirect_uri;
+  function rally($q, $http, $cordovaOauthUtility) {
+    return { signin: oauthRally };
+
+    /*
+     * Sign into the Rally service
+     *
+     * @param    string clientId
+     * @param    string clientSecret
+     * @param    string appScope
+     * @param    object options
+     * @return   promise
+     */
+    function oauthRally(clientId, clientSecret, appScope, options) {
+      var deferred = $q.defer();
+      if(window.cordova) {
+          if($cordovaOauthUtility.isInAppBrowserInstalled()) {
+            var redirect_uri = "http://localhost/callback";
+            if(options !== undefined) {
+              if(options.hasOwnProperty("redirect_uri")) {
+                redirect_uri = options.redirect_uri;
+              }
             }
+            var browserRef = window.cordova.InAppBrowser.open('https://rally1.rallydev.com/login/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope + '&response_type=code', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+            browserRef.addEventListener('loadstart', function(event) {
+              if((event.url).indexOf("http://localhost/callback") === 0) {
+                requestToken = (event.url).split("code=")[1];
+                $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+                $http({method: "post", url: "https://rally1.rallydev.com/login/oauth2/auth", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirect_uri + "&grant_type=authorization_code" + "&code=" + requestToken })
+                  .success(function(data) {
+                    deferred.resolve(data);
+                  })
+                  .error(function(data, status) {
+                    deferred.reject("Problem authenticating");
+                  })
+                  .finally(function() {
+                    setTimeout(function() {
+                      browserRef.close();
+                    }, 10);
+                  });
+              }
+            });
+            browserRef.addEventListener('exit', function(event) {
+              deferred.reject("The sign in flow was canceled");
+            });
+          } else {
+            deferred.reject("Could not find InAppBrowser plugin");
           }
-          var browserRef = window.cordova.InAppBrowser.open('https://rally1.rallydev.com/login/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope + '&response_type=code', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-          browserRef.addEventListener('loadstart', function(event) {
-            if((event.url).indexOf("http://localhost/callback") === 0) {
-              requestToken = (event.url).split("code=")[1];
-              $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-              $http({method: "post", url: "https://rally1.rallydev.com/login/oauth2/auth", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirect_uri + "&grant_type=authorization_code" + "&code=" + requestToken })
-                .success(function(data) {
-                  deferred.resolve(data);
-                })
-                .error(function(data, status) {
-                  deferred.reject("Problem authenticating");
-                })
-                .finally(function() {
-                  setTimeout(function() {
-                    browserRef.close();
-                  }, 10);
-                });
-            }
-          });
-          browserRef.addEventListener('exit', function(event) {
-            deferred.reject("The sign in flow was canceled");
-          });
-        } else {
-          deferred.reject("Could not find InAppBrowser plugin");
-        }
-    } else {
-      deferred.reject("Cannot authenticate via a web browser");
+      } else {
+        deferred.reject("Cannot authenticate via a web browser");
+      }
+
+      return deferred.promise;
     }
-
-    return deferred.promise;
   }
-}
 
-rally.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+  rally.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+})();
