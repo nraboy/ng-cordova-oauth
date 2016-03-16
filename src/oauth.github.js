161e4e25b35625812f75e1cbdced6b23484f913b
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2016-03-16 14:55:50 -0300

    IIFE github

--- src/oauth.github.js
+++ src/oauth.github.js
@@ -1,60 +1,64 @@
-angular.module('oauth.github', ['oauth.utils'])
-  .factory('$ngCordovaGithub', github);
+(function() {
+  'use strict';
 
-function github($q, $http, $cordovaOauthUtility) {
-  return { signin: oauthGithub };
+  angular.module('oauth.github', ['oauth.utils'])
+    .factory('$ngCordovaGithub', github);
 
-  /*
-   * Sign into the GitHub service
-   *
-   * @param    string clientId
-   * @param    string clientSecret
-   * @param    array appScope
-   * @param    object options
-   * @return   promise
-   */
-  function oauthGithub(clientId, clientSecret, appScope, options) {
-    var deferred = $q.defer();
-    if(window.cordova) {
-      if($cordovaOauthUtility.isInAppBrowserInstalled()) {
-        var redirect_uri = "http://localhost/callback";
-        if(options !== undefined) {
-          if(options.hasOwnProperty("redirect_uri")) {
-            redirect_uri = options.redirect_uri;
+  function github($q, $http, $cordovaOauthUtility) {
+    return { signin: oauthGithub };
+
+    /*
+     * Sign into the GitHub service
+     *
+     * @param    string clientId
+     * @param    string clientSecret
+     * @param    array appScope
+     * @param    object options
+     * @return   promise
+     */
+    function oauthGithub(clientId, clientSecret, appScope, options) {
+      var deferred = $q.defer();
+      if(window.cordova) {
+        if($cordovaOauthUtility.isInAppBrowserInstalled()) {
+          var redirect_uri = "http://localhost/callback";
+          if(options !== undefined) {
+            if(options.hasOwnProperty("redirect_uri")) {
+              redirect_uri = options.redirect_uri;
+            }
           }
+          var browserRef = window.cordova.InAppBrowser.open('https://github.com/login/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(","), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+          browserRef.addEventListener('loadstart', function(event) {
+            if((event.url).indexOf(redirect_uri) === 0) {
+              requestToken = (event.url).split("code=")[1];
+              $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+              $http.defaults.headers.post.accept = 'application/json';
+              $http({method: "post", url: "https://github.com/login/oauth/access_token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirect_uri + "&code=" + requestToken })
+                .success(function(data) {
+                  deferred.resolve(data);
+                })
+                .error(function(data, status) {
+                  deferred.reject("Problem authenticating");
+                })
+                .finally(function() {
+                  setTimeout(function() {
+                      browserRef.close();
+                  }, 10);
+                });
+            }
+          });
+          browserRef.addEventListener('exit', function(event) {
+              deferred.reject("The sign in flow was canceled");
+          });
+        } else {
+            deferred.reject("Could not find InAppBrowser plugin");
         }
-        var browserRef = window.cordova.InAppBrowser.open('https://github.com/login/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(","), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-        browserRef.addEventListener('loadstart', function(event) {
-          if((event.url).indexOf(redirect_uri) === 0) {
-            requestToken = (event.url).split("code=")[1];
-            $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-            $http.defaults.headers.post.accept = 'application/json';
-            $http({method: "post", url: "https://github.com/login/oauth/access_token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&redirect_uri=" + redirect_uri + "&code=" + requestToken })
-              .success(function(data) {
-                deferred.resolve(data);
-              })
-              .error(function(data, status) {
-                deferred.reject("Problem authenticating");
-              })
-              .finally(function() {
-                setTimeout(function() {
-                    browserRef.close();
-                }, 10);
-              });
-          }
-        });
-        browserRef.addEventListener('exit', function(event) {
-            deferred.reject("The sign in flow was canceled");
-        });
       } else {
-          deferred.reject("Could not find InAppBrowser plugin");
+        deferred.reject("Cannot authenticate via a web browser");
       }
-    } else {
-      deferred.reject("Cannot authenticate via a web browser");
+
+      return deferred.promise;
     }
-
-    return deferred.promise;
   }
-}
 
-github.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+  github.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+})();
