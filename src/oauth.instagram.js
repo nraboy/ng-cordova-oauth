dc9bbf105baf11b6c7fc23f4ec83df17082fab0d
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2016-03-16 15:03:06 -0300

    IIFE instagram

--- src/oauth.instagram.js
+++ src/oauth.instagram.js
@@ -1,70 +1,74 @@
-angular.module('oauth.instagram', ['oauth.utils'])
-  .factory('$ngCordovaInstagram', instagram);
+(function() {
+  'use strict';
 
-function instagram($q, $http, $cordovaOauthUtility) {
-  return { signin: oauthInstagram };
+  angular.module('oauth.instagram', ['oauth.utils'])
+    .factory('$ngCordovaInstagram', instagram);
 
-  /*
-   * Sign into the Instagram service
-   *
-   * @param    string clientId
-   * @param    array appScope
-   * @param    object options
-   * @return   promise
-   */
-  function oauthInstagram(clientId, appScope, options) {
-    var deferred = $q.defer();
-    var split_tokens = {
-        'code':'?',
-        'token':'#'
-    };
+  function instagram($q, $http, $cordovaOauthUtility) {
+    return { signin: oauthInstagram };
 
-    if(window.cordova) {
-      if($cordovaOauthUtility.isInAppBrowserInstalled()) {
-        var redirect_uri = "http://localhost/callback";
-        var response_type = "token";
-        if(options !== undefined) {
-          if(options.hasOwnProperty("redirect_uri")) {
-            redirect_uri = options.redirect_uri;
+    /*
+     * Sign into the Instagram service
+     *
+     * @param    string clientId
+     * @param    array appScope
+     * @param    object options
+     * @return   promise
+     */
+    function oauthInstagram(clientId, appScope, options) {
+      var deferred = $q.defer();
+      var split_tokens = {
+          'code':'?',
+          'token':'#'
+      };
+
+      if(window.cordova) {
+        if($cordovaOauthUtility.isInAppBrowserInstalled()) {
+          var redirect_uri = "http://localhost/callback";
+          var response_type = "token";
+          if(options !== undefined) {
+            if(options.hasOwnProperty("redirect_uri")) {
+              redirect_uri = options.redirect_uri;
+            }
+            if(options.hasOwnProperty("response_type")) {
+              response_type = options.response_type;
+            }
           }
-          if(options.hasOwnProperty("response_type")) {
-            response_type = options.response_type;
+
+          var scope = '';
+          if (appScope && appScope.length > 0) {
+            scope = '&scope' + appScope.join('+');
           }
+
+          var browserRef = window.cordova.InAppBrowser.open('https://api.instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirect_uri + scope + '&response_type='+response_type, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+          browserRef.addEventListener('loadstart', function(event) {
+            if((event.url).indexOf(redirect_uri) === 0) {
+                browserRef.removeEventListener("exit",function(event){});
+                browserRef.close();
+                var callbackResponse = (event.url).split(split_tokens[response_type])[1];
+                var parameterMap = $cordovaOauthUtility.parseResponseParameters(callbackResponse);
+                if(parameterMap.access_token) {
+                  deferred.resolve({ access_token: parameterMap.access_token });
+                } else if(parameterMap.code !== undefined && parameterMap.code !== null) {
+                  deferred.resolve({ code: parameterMap.code });
+                } else {
+                  deferred.reject("Problem authenticating");
+                }
+            }
+          });
+          browserRef.addEventListener('exit', function(event) {
+              deferred.reject("The sign in flow was canceled");
+          });
+        } else {
+            deferred.reject("Could not find InAppBrowser plugin");
         }
-
-        var scope = '';
-        if (appScope && appScope.length > 0) {
-          scope = '&scope' + appScope.join('+');
-        }
-
-        var browserRef = window.cordova.InAppBrowser.open('https://api.instagram.com/oauth/authorize/?client_id=' + clientId + '&redirect_uri=' + redirect_uri + scope + '&response_type='+response_type, '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-        browserRef.addEventListener('loadstart', function(event) {
-          if((event.url).indexOf(redirect_uri) === 0) {
-              browserRef.removeEventListener("exit",function(event){});
-              browserRef.close();
-              var callbackResponse = (event.url).split(split_tokens[response_type])[1];
-              var parameterMap = $cordovaOauthUtility.parseResponseParameters(callbackResponse);
-              if(parameterMap.access_token) {
-                deferred.resolve({ access_token: parameterMap.access_token });
-              } else if(parameterMap.code !== undefined && parameterMap.code !== null) {
-                deferred.resolve({ code: parameterMap.code });
-              } else {
-                deferred.reject("Problem authenticating");
-              }
-          }
-        });
-        browserRef.addEventListener('exit', function(event) {
-            deferred.reject("The sign in flow was canceled");
-        });
       } else {
-          deferred.reject("Could not find InAppBrowser plugin");
+        deferred.reject("Cannot authenticate via a web browser");
       }
-    } else {
-      deferred.reject("Cannot authenticate via a web browser");
+
+      return deferred.promise;
     }
-
-    return deferred.promise;
   }
-}
 
-instagram.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+  instagram.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+})();
