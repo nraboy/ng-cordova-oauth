3b9d218d4d9ee390e1a5d56ad7cd8184d3cf5bd5
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2016-03-16 15:26:48 -0300

    IIFE uber

--- src/oauth.uber.js
+++ src/oauth.uber.js
@@ -1,61 +1,65 @@
-angular.module('oauth.uber', ['oauth.utils'])
-  .factory('$ngCordovaUber', uber);
+(function() {
+  'use strict';
 
-function uber($q, $http, $cordovaOauthUtility) {
-  return { signin: oauthUber };
+  angular.module('oauth.uber', ['oauth.utils'])
+    .factory('$ngCordovaUber', uber);
 
-  /*
-   * Sign into the Uber service
-   *
-   * @param    string clientId
-   * @param    appScope array
-   * @param    object options
-   * @return   promise
-   */
-  function oauthUber(clientId, appScope, options) {
+  function uber($q, $http, $cordovaOauthUtility) {
+    return { signin: oauthUber };
 
-    var deferred = $q.defer();
-    if(window.cordova) {
+    /*
+     * Sign into the Uber service
+     *
+     * @param    string clientId
+     * @param    appScope array
+     * @param    object options
+     * @return   promise
+     */
+    function oauthUber(clientId, appScope, options) {
 
-      if($cordovaOauthUtility.isInAppBrowserInstalled()) {
-        var redirect_uri = "http://localhost/callback";
-        if(options !== undefined) {
-          if(options.hasOwnProperty("redirect_uri")) {
-            redirect_uri = options.redirect_uri;
+      var deferred = $q.defer();
+      if(window.cordova) {
+
+        if($cordovaOauthUtility.isInAppBrowserInstalled()) {
+          var redirect_uri = "http://localhost/callback";
+          if(options !== undefined) {
+            if(options.hasOwnProperty("redirect_uri")) {
+              redirect_uri = options.redirect_uri;
+            }
           }
+          var browserRef = window.cordova.InAppBrowser.open('https://login.uber.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&response_type=token&scope=' + appScope.join(" "), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+          browserRef.addEventListener('loadstart', function(event) {
+            if((event.url).indexOf(redirect_uri) === 0) {
+              browserRef.removeEventListener("exit",function(event){});
+              browserRef.close();
+              var callbackResponse = (event.url).split("#")[1];
+              var responseParameters = (callbackResponse).split("&");
+              var parameterMap = [];
+
+              for(var i = 0; i < responseParameters.length; i++) {
+                parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+              }
+
+              if(parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
+                deferred.resolve({ access_token: parameterMap.access_token, token_type: parameterMap.token_type, expires_in: parameterMap.expires_in, scope: parameterMap.scope });
+              } else {
+                deferred.reject("Problem authenticating");
+              }
+            }
+          });
+          browserRef.addEventListener('exit', function(event) {
+            deferred.reject("The sign in flow was canceled");
+          });
+        } else {
+          deferred.reject("Could not find InAppBrowser plugin");
         }
-        var browserRef = window.cordova.InAppBrowser.open('https://login.uber.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&response_type=token&scope=' + appScope.join(" "), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
-        browserRef.addEventListener('loadstart', function(event) {
-          if((event.url).indexOf(redirect_uri) === 0) {
-            browserRef.removeEventListener("exit",function(event){});
-            browserRef.close();
-            var callbackResponse = (event.url).split("#")[1];
-            var responseParameters = (callbackResponse).split("&");
-            var parameterMap = [];
-
-            for(var i = 0; i < responseParameters.length; i++) {
-              parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
-            }
-
-            if(parameterMap.access_token !== undefined && parameterMap.access_token !== null) {
-              deferred.resolve({ access_token: parameterMap.access_token, token_type: parameterMap.token_type, expires_in: parameterMap.expires_in, scope: parameterMap.scope });
-            } else {
-              deferred.reject("Problem authenticating");
-            }
-          }
-        });
-        browserRef.addEventListener('exit', function(event) {
-          deferred.reject("The sign in flow was canceled");
-        });
       } else {
-        deferred.reject("Could not find InAppBrowser plugin");
+        deferred.reject("Cannot authenticate via a web browser");
       }
-    } else {
-      deferred.reject("Cannot authenticate via a web browser");
+      return deferred.promise;
+
     }
-    return deferred.promise;
-
   }
-}
 
-uber.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+  uber.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+})();
