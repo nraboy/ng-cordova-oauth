cddb6869d56c9a7014ebaa4ecd17fbfbfee8e44c
Ben Buie <mail@benbuie.com>
2016-05-03 17:52:12 -0600

    starting to add netatmo

---
+++ src/oauth.netatmo.js
@@ -0,0 +1,65 @@
+(function() {
+  'use strict';
+
+  angular.module('oauth.netatmo', ['oauth.utils']).factory('$ngCordovaNetatmo', netatmo);
+
+  function netatmo($q, $http, $cordovaOauthUtility) {
+    return { signin: oauthNetatmo };
+
+    /*
+     * Sign into the Netatmo service
+     *
+     * @param    string clientId
+     * @param    string clientSecret
+     * @param    string appScope
+     * @param    object options
+     * @return   promise
+     */
+    function oauthNetatmo(clientId,clientSecret, appScope, options) {
+      var deferred = $q.defer();
+      if(window.cordova) {
+        if($cordovaOauthUtility.isInAppBrowserInstalled()) {
+          var redirect_uri = "http://localhost/callback";
+          if(options !== undefined) {
+            if(options.hasOwnProperty("redirect_uri")) {
+              redirect_uri = options.redirect_uri;
+            }
+          }
+          var browserRef = window.cordova.InAppBrowser.open('https://netatmo.com/auth/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&response_type=code&scope=' + appScope.join(" "), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+
+          browserRef.addEventListener('loadstart', function(event) {
+            if((event.url).indexOf(redirect_uri) === 0) {
+              var requestToken = (event.url).split("code=")[1];
+
+              $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+              $http({method: "post", url: "https://netatmo.com/auth/oauth2/token", data: "client_id=" + clientId + "&client_secret=" + clientSecret + "&grant_type=authorization_code&code=" + requestToken })
+                .success(function(data) {
+                  deferred.resolve(data);
+                })
+                .error(function(data, status) {
+                  deferred.reject("Problem authenticating");
+                })
+                .finally(function() {
+                  setTimeout(function() {
+                    browserRef.close();
+                  }, 10);
+                });
+
+            }
+          });
+          browserRef.addEventListener('exit', function(event) {
+            deferred.reject("The sign in flow was canceled");
+          });
+        } else {
+          deferred.reject("Could not find InAppBrowser plugin");
+        }
+      } else {
+        deferred.reject("Cannot authenticate via a web browser");
+      }
+
+      return deferred.promise;
+    }
+  }
+
+  netatmo.$inject = ['$q', '$http', '$cordovaOauthUtility'];
+})();
